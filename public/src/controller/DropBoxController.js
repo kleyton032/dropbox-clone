291538e6f2cc5dc609c479f97097b9223801a614
Kleyton João <kleyton.joao@gmail.com>
2019-04-20 16:04:16 -0300

    Controller criado e click do button enviar arquivos

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,4 +1,22 @@
 class DropBoxController {
 
-    constructor(){}
+    constructor(){
+        this.buttonSendFile = document.querySelector('#btn-send-file');
+        this.inputSendFile = document.querySelector('#files');
+        this.snackModal = document.querySelector('#react-snackbar-root')
+        this.initEvents();
+    }
+
+
+    initEvents(){
+        this.buttonSendFile.addEventListener('click', event => {
+            this.inputSendFile.click();
+        });
+
+        this.inputSendFile.addEventListener('change', event =>{
+            console.log(event.target.files)
+            this.snackModal.style.display = 'block'
+        });
+    }
+
 } 
